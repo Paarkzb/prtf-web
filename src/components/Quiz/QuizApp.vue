e93120c54fa437c7e85364dfa199cf6708d2d791
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2024-04-23 00:25:29 +0800

    added add-quiz function

--- src/components/Quiz/QuizApp.vue
+++ src/components/Quiz/QuizApp.vue
@@ -1,9 +1,70 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
 import QuizQuestion from './QuizQuestion.vue'
+import axios from 'axios'
+
+const id = ref(1)
+
+declare interface question {
+  id: number
+  question: string
+  answer: string
+}
+
+const quizes: Ref<question[]> = ref([])
+
+function addQuiz() {
+  quizes.value.push({
+    id: id.value++,
+    question: '',
+    answer: ''
+  })
+}
+
+function saveQuiz() {
+  axios
+    .post('http://localhost:8001/quiz', {
+      quizes: quizes.value
+    })
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
 </script>
 
 <template>
   <div>
-    <QuizQuestion />
+    {{ quizes }}
+    <form>
+      <div class="mb-5">
+        <input
+          type="button"
+          value="Добавить вопрос"
+          @click="addQuiz"
+          id="add-quiz-button"
+          name="add-quiz-button"
+        />
+      </div>
+      <div v-for="quiz in quizes" :key="quiz.id">
+        <QuizQuestion
+          :Id="quiz.id"
+          @question="(msg) => (quizes[quiz.id - 1].question = msg)"
+          @answer="(msg) => (quizes[quiz.id - 1].answer = msg)"
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          @click="saveQuiz"
+          value="Сохранить квиз"
+          id="save-quiz-button"
+          name="save-quiz-button"
+        />
+      </div>
+    </form>
   </div>
 </template>
--- src/components/Quiz/QuizQuestion.vue
+++ src/components/Quiz/QuizQuestion.vue
@@ -1,12 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  Id: Number
+})
+
+const emit = defineEmits(['question', 'answer'])
+
+const questionValue = ref('')
+const answerValue = ref('')
+
+function questionChange(e) {
+  questionValue.value = e.target.value
+  emit('question', questionValue)
+}
+
+function answerChange(e) {
+  answerValue.value = e.target.value
+  emit('answer', answerValue)
+}
+</script>
+
 <template>
   <div>
     <div class="mb-5 flex gap-x-5">
       <label for="question">Вопрос</label>
-      <input type="text" name="question" />
+      <input
+        type="text"
+        name="question"
+        @input="questionChange"
+        :id="'question' + Id"
+        :value="questionValue"
+      />
     </div>
     <div class="mb-5 flex gap-x-5">
       <label for="answer">Ответ</label>
-      <input type="text" name="answer" />
+      <input
+        type="text"
+        name="answer"
+        @input="answerChange"
+        :id="'answer' + Id"
+        :value="answerValue"
+      />
     </div>
   </div>
 </template>
